1dc5bd77155bd405510893d826ccb90c752b74ec
Paige Sun <paige.sun.dev@gmail.com>
2023-04-18 12:21:17 -0700

    feat: Make it work on Stackblitz.

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@
 import { createFluidSim } from './src/FluidSim';
 import Vec2 from './src/Utils/Vec2';
 
+const size = Math.min(window.innerWidth - 40, window.innerHeight - 180);
 const fluidSim = createFluidSim({
   initialScene: 'Latte Scene',
   canvasDomId: 'myCanvas',
   buttonsDomId: 'inputDiv',
-  canvasSize: new Vec2(window.innerHeight - 240, window.innerHeight - 270),
+  canvasSize: new Vec2(size, size),
   resolutionOverride: undefined,
   autostart: true,
 });

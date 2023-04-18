421183ae6d0965c94d40c1907bfc6970dc1dd213
Paige Sun <paige.sun.dev@gmail.com>
2023-04-18 12:33:43 -0700

    ux: Switch copy on "Milk" vs "Pen". Window size.

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const fluidSim = createFluidSim({
   initialScene: 'Latte Scene',
   canvasDomId: 'myCanvas',
   buttonsDomId: 'inputDiv',
-  canvasSize: new Vec2(window.innerHeight - 240, window.innerHeight - 270),
+  canvasSize: new Vec2(window.innerWidth - 80, window.innerHeight - 270),
   resolutionOverride: undefined,
   autostart: true,
 });

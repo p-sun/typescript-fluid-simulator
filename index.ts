21cde73ed9fd9c4b22e7011d1e9d1fb62be4d206
Paige Sun <paige.sun.dev@gmail.com>
2023-04-19 16:26:38 -0700

    feat: Make it work for StackBlitz.

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const fluidSim = createFluidSim({
   initialSceneTag: 'Latte Scene',
   canvasDomId: 'myCanvas',
   inputDomId: 'inputDiv',
-  canvasSize: new Vec2(window.innerWidth - 80, window.innerHeight - 270),
+  canvasSize: new Vec2(window.innerWidth - 42, window.innerHeight - 180),
   autostart: true,
   overrides: {},
 });
